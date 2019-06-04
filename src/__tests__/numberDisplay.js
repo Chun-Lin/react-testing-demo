1c54c84cc30349f3f248a3dc1c2bc7f7231e257d
Chun-Lin <wulin40063@gmail.com>
2019-06-04 11:57:15 +0800

    test: 💍 add <NumberDisplay> snapshot testing

--- src/__tests__/numberDisplay.js
+++ src/__tests__/numberDisplay.js
@@ -11,5 +11,9 @@ test('calling render with the same component on the same container does not remo
   // re-render the same component with different props
   rerender(<NumberDisplay number={2} />)
   expect(getByTestId('number-display').textContent).toBe('2')
+})
 
+test('<NumberDisplay> snapshot', () => {
+  const { container } = render(<NumberDisplay number={1} />)
+  expect(container).toMatchSnapshot()
 })
